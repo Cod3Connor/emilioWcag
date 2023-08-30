6710552c544f1fa673f572d6f5d0e032dbef5eb1
Lio Pena <emilio.pena@nwktc.edu>
2023-08-30 10:15:19 -0600

    Added buttons

---
+++ emilio.js
@@ -0,0 +1,11 @@
+function goToSpot() {
+    console.log("Going to Spot");
+}
+
+function goToStats() {
+    console.log("Going to Stats");
+}
+
+function goToReport() {
+    console.log("Going to Report");
+}
